2b2316abda0bb71e67659b7a0bef269c78835e3b
Kavic07 <kavicblessing@gmail.com>
2025-06-03 22:10:12 +0100

    Add delete task button

--- tasktracker.js
+++ tasktracker.js
@@ -1,1 +1,17 @@
-console.log("Loading task tracker");
+function addTask() {
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  if (taskInput.value !== "") {
+    const li = document.createElement("li");
+    li.textContent = taskInput.value;
+
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+    delBtn.onclick = () => li.remove();
+
+    li.appendChild(delBtn);
+    taskList.appendChild(li);
+    taskInput.value = "";
+  }
+}
