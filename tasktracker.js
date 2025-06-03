9b6e5949827ad7ddef75845c36f7349da3dcbcdc
Kavic07 <kavicblessing@gmail.com>
2025-06-03 21:54:13 +0100

    Add task creation feature

--- tasktracker.js
+++ tasktracker.js
@@ -1,1 +1,11 @@
-console.log("Loading task tracker");
+function addTask() {
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  if (taskInput.value !== "") {
+    const li = document.createElement("li");
+    li.textContent = taskInput.value;
+    taskList.appendChild(li);
+    taskInput.value = "";
+  }
+}
